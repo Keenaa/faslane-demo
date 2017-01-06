01cc16d1fb65a3abff5775655de2cfe195483162
Meryl Barantal <meryl2194@gmail.com>
2017-01-06 10:00:24 +0100

    first

---
+++ User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  FastLaneDemo
+//
+//  Created by Meryl Barantal on 06/01/2017.
+//  Copyright © 2017 Meryl Barantal. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
